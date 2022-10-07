fdf46c721c3b7308dfe91aa8b6d6d82550eea38f
hackboxguy <albert.david@gmail.com>
2022-10-07 21:56:39 +0200

    updated init-remote-box.sh for gl-mt300nv2-awsiot-demo config

--- configs/gl-mt300nv2-awsiot-demo/rootfs_overlay/usr/sbin/init-remote-box.sh
+++ configs/gl-mt300nv2-awsiot-demo/rootfs_overlay/usr/sbin/init-remote-box.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 #this script overrides default settings of an openwrt system with provided custom data.
-#custom data is used to set hostname/rootpw/wifissid/wifipw/xmppuser/xmpppw
-#e.g: init-remote-box.sh --name=remote-kit-001 --xmpusr=remote-kit-001@ubuntu-jabber.de --xmppw=remote-kit-pw
+#custom data is used to set hostname/rootpw/wifissid/wifipw
+#e.g: init-remote-box.sh --ssidhostname=aws-iot-demo-001 --systempw=supersecretpw
 USAGE="usage: $0 script user for overriding default settings of openwrt-box
-usage: $0 --name=remote-kit-001 --syspw=pw --xmpusr=remote-kit-001@ubuntu-jabber.de --xmppw=remote-kit-pw"
+usage: $0 --ssidhostname=aws-iot-demo-001 --systempw=supersecretpw
+Note: reboot is required after invoking this script"
 
 NOARGS="yes"
 NAME="none"
-XMPUSR="none"
-XMPPW="none"
 SYSPW="none"
 ###############################################################################
 optspec=":h-:"
@@ -16,25 +15,13 @@ while getopts "$optspec" optchar; do
     case "${optchar}" in
         -)
             case "${OPTARG}" in
-                    name=*) #--name=value
+                    ssidhostname=*) #--name=value
                         val=${OPTARG#*=}
                         opt=${OPTARG%=$val}
                         NAME=${val}
                         NOARGS="no"
                         ;;
-                    xmpuser=*)
-                        val=${OPTARG#*=}
-                        opt=${OPTARG%=$val}
-                        XMPUSR=${val}
-                        NOARGS="no"
-                        ;;
-                    xmppw=*)
-                        val=${OPTARG#*=}
-                        opt=${OPTARG%=$val}
-                        XMPPW=${val}
-                        NOARGS="no"
-                        ;;
-                    syspw=*)
+                    systempw=*)
                         val=${OPTARG#*=}
                         opt=${OPTARG%=$val}
                         SYSPW=${val}
@@ -82,63 +69,29 @@ if [ $SYSPW = "none" ]; then
     exit 1
 fi
 
-#incase if --xmpuser arg is not provided, then use --name arg as xmpuser
-if [ $XMPUSR = "none" ]; then
-    XMPUSR=$NAME
-fi
-
-#incase if --xmppw arg is not provided, then use --name arg as xmppw
-if [ $XMPPW = "none" ]; then
-    XMPPW=$NAME
-fi
-
+#set wifi ssid
 uci set "wireless.default_radio0.ssid=$NAME"
 [ $? != 0 ] && echo "Failed to set ssid" && exit 1
+
+#set wifi password
 uci set "wireless.default_radio0.key=$SYSPW"
 [ $? != 0 ] && echo "Failed to set ssidpw" && exit 1
+
+#set hostname
 uci set "system.@system[0].hostname=$NAME"
 [ $? != 0 ] && echo "Failed to set hostname" && exit 1
 
+#set root-password
 echo -e "$SYSPW\n$SYSPW" | passwd root 1>/dev/nulll 2>/dev/null
 [ $? != 0 ] && echo "Failed to change root pw" && exit 1
-mkdir -p /etc/xmproxy/
-echo "user: $XMPUSR" >/etc/xmproxy/xmpp-login.txt;
-[ $? != 0 ] && echo "Failed to write xmpp username to file" && exit 1
-echo "pw: $XMPPW">>/etc/xmproxy/xmpp-login.txt
-[ $? != 0 ] && echo "Failed to write xmpp password to file" && exit 1
 
-#finally set a flag so that /etc/uci-defaults doesnt trigger after sysupgrade
+#finally set a flag so that /etc/uci-defaults doesnt get invoked again after sysupgrade
 uci set "system.@system[0].initremotebox=yes"
 uci commit
 [ $? != 0 ] && echo "Failed to commit uci settings" && exit 1
 
-#init other settings
+#create dropbear-authorized-key incase of secure-reverse-tunneling required from a remote server
 mkdir -p /root/.ssh
 dropbearkey -f /root/.ssh/id_rsa -t rsa -s 2048 1>/dev/null 2>/dev/null
-echo "on=sonoff \$switch1 on" > /etc/xmproxy/xmpp-alias-list.txt
-echo "off=sonoff \$switch1 off" >> /etc/xmproxy/xmpp-alias-list.txt
-echo "sshstop=shellcmd /usr/bin/ssh-stop.sh" >> /etc/xmproxy/xmpp-alias-list.txt
-echo "sshstatus=shellcmd /usr/bin/ssh-running.sh" >> /etc/xmproxy/xmpp-alias-list.txt
-echo "sshstart=shellcmd /usr/bin/ssh-start.sh \$ip \$tunnelport \$sshport &" >> /etc/xmproxy/xmpp-alias-list.txt
-echo "sshkey=shellcmd dropbearkey -y -f /root/.ssh/id_rsa;sleep 2;shellcmdresp" >> /etc/xmproxy/xmpp-alias-list.txt
-echo "sshkeyclean=shellcmd rm -rf /root/.ssh/id_rsa" >> /etc/xmproxy/xmpp-alias-list.txt
-echo "sshkeynew=shellcmdtrig rm -rf /root/.ssh/;shellcmdtrig mkdir -p /root/.ssh/;shellcmdtrig dropbearkey -f /root/.ssh/id_rsa -t rsa -s 2048" >> /etc/xmproxy/xmpp-alias-list.txt
-echo "switch1=ipoftasmota" >> /etc/xmproxy/xmpp-alias-list.txt
-echo "tunnelport=4202" >> /etc/xmproxy/xmpp-alias-list.txt
-echo "sshport=55611" >> /etc/xmproxy/xmpp-alias-list.txt
-echo "ip=remote-ssh-server-ip" >> /etc/xmproxy/xmpp-alias-list.txt
-echo "dropbearon=shellcmdtrig /etc/init.d/dropbear start" >> /etc/xmproxy/xmpp-alias-list.txt
-echo "dropbearoff=shellcmdtrig /etc/init.d/dropbear stop" >> /etc/xmproxy/xmpp-alias-list.txt
-echo "dropbearsts=shellcmd /usr/bin/dropbear-running.sh" >> /etc/xmproxy/xmpp-alias-list.txt
-echo "dhcplist=shellcmd /usr/bin/dhcp-list.sh;sleep 2;shellcmdresp" >> /etc/xmproxy/xmpp-alias-list.txt
-echo "datacount=shellcmd /usr/sbin/data-usage.sh \$dataiface ;sleep 1;shellcmdresp" >> /etc/xmproxy/xmpp-alias-list.txt
-echo "dataiface=br-lan" >> /etc/xmproxy/xmpp-alias-list.txt
-echo "interfaces=shellcmd /usr/sbin/show-interfaces.sh;sleep 2;shellcmdresp" >> /etc/xmproxy/xmpp-alias-list.txt
-echo "usbon=shellcmdtrig /usr/sbin/usb-port-power.sh on" >> /etc/xmproxy/xmpp-alias-list.txt
-echo "usboff=shellcmdtrig /usr/sbin/usb-port-power.sh off" >> /etc/xmproxy/xmpp-alias-list.txt
-echo "usbsts=shellcmd /usr/sbin/usb-port-power.sh;sleep 1;shellcmdresp" >> /etc/xmproxy/xmpp-alias-list.txt
-#usboff=shellcmdtrig echo 0 > /sys/class/gpio/usb/value
-#usbon=shellcmdtrig echo 1 > /sys/class/gpio/usb/value
-#xmpoff=shellcmdtrig /opt/fmw/bin/xmproxyclt --shutdown
 
 echo "Result: Success"

747aa1c6e1028f8f5d545b1cc94e8bb63b4909e8
hackboxguy <albert.david@gmail.com>
2022-10-09 19:26:15 +0200

    added lua based webUI components for AWS-IoT-Demo image

---
+++ configs/gl-mt300nv2-awsiot-demo/rootfs_overlay/usr/lib/lua/luci/model/cbi/awsiot_cbi/awsiot-srvice-settings.lua
@@ -0,0 +1,10 @@
+m = Map("awsiot-demo", translate("AWS-IoT-Demo Settings"), translate("")) -- awsiot-demo is the config file in /etc/config
+d = m:section(TypedSection, "info", "")  -- info is the section called info in awsiot-demo file
+a = d:option(Value, "endpoint", "Endpoint"); a.optional=false; a.rmempty = false;  -- endpoint is the option in the awsiot-demo file
+b = d:option(Value, "clientid", "ClientID"); b.optional=false; b.rmempty = false; 
+c = d:option(Value, "topic", "Publish Topic"); c.optional=false; c.rmempty = false;
+e = d:option(Value, "interval", "Publish Inverval Sec"); e.optional=false; e.rmempty = false;
+f = d:option(Value, "count", "Publish Count"); f.optional=false; f.rmempty = false;
+g = d:option(Value, "verbosity", "Debug Verbosity"); g.optional=false; g.rmempty = false;
+h = d:option(Value, "message", "Publish Message"); h.optional=false; h.rmempty = false;
+return m

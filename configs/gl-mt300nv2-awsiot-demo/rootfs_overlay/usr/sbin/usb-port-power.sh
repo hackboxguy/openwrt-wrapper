28fe7b936735761caf25ac725f46dc0f084b74d8
hackboxguy <albert.david@gmail.com>
2022-10-12 22:17:28 +0200

    added json file as argument for led-power.sh and usb-port-power.sh scripts

--- configs/gl-mt300nv2-awsiot-demo/rootfs_overlay/usr/sbin/usb-port-power.sh
+++ configs/gl-mt300nv2-awsiot-demo/rootfs_overlay/usr/sbin/usb-port-power.sh
@@ -7,15 +7,31 @@ USB_NODE="none"
 [ -f  "$USB_NODE2" ] && USB_NODE=$USB_NODE2
 [ "$USB_NODE" = "none" ] && echo "usb node not found!!!!" && return 1
 
+#read action
 if [ -z $1 ]; then
 	VAL=$(cat $USB_NODE)
 	[ $VAL = "0" ] && echo "off" && return 0
 	[ $VAL = "1" ] && echo "on" && return 0
 	echo "usb-power: unknown value" && return 1
-elif [ $1 = "on" ]; then
+fi
+
+if [ -f $1 ]; then
+	#check if argument $1 is a json file
+	JQOBJ=$(jq type < $1)
+	if [ $? = "0" ]; then #this is a valid json file
+		MYARG=$(jq -r .powerstate $1)
+	else
+		MYARG=$1 #just take argument as on/off string
+	fi
+else
+	MYARG=$1
+fi
+
+#write action
+if [ $MYARG = "on" ]; then
 	echo 1 > $USB_NODE
 	return 0
-elif [ $1 = "off" ]; then
+elif [ $MYARG = "off" ]; then
 	echo 0 > $USB_NODE
 	return 0
 else

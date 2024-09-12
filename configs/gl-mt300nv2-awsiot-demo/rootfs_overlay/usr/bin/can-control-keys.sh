64aeb00ee94586c038b35fcf9c7a483b1e1ada11
hackboxguy <albert.david@gmail.com>
2024-09-12 16:08:07 +0200

    updated car-can-emulator trigger keys

--- configs/gl-mt300nv2-awsiot-demo/rootfs_overlay/usr/bin/can-control-keys.sh
+++ configs/gl-mt300nv2-awsiot-demo/rootfs_overlay/usr/bin/can-control-keys.sh
@@ -1,42 +1,71 @@
 #!/bin/sh
 case $1 in
-   #KEY_PAGEUP)
    KEY_0)
 	VAL=$(echo -n "speed" | nc 127.0.0.1 8080)
 	VAL=$(($VAL+1))
 	echo -n "speed $VAL" | nc 127.0.0.1 8080
 	;;
-   #KEY_PAGEDOWN)
    KEY_1)
 	VAL=$(echo -n "speed" | nc 127.0.0.1 8080)
 	VAL=$(($VAL-1))
 	echo -n "speed $VAL" | nc 127.0.0.1 8080
 	;;
-   #KEY_UP)
    KEY_2)
 	VAL=$(echo -n "rpm" | nc 127.0.0.1 8080)
 	VAL=$(($VAL+1))
 	echo -n "rpm $VAL" | nc 127.0.0.1 8080
       ;;
-   #KEY_DOWN)
    KEY_3)
 	VAL=$(echo -n "rpm" | nc 127.0.0.1 8080)
 	VAL=$(($VAL-1))
 	echo -n "rpm $VAL" | nc 127.0.0.1 8080
       ;;
-   #KEY_VOLUMEUP)
    KEY_4)
 	VAL=$(echo -n "flow" | nc 127.0.0.1 8080)
 	VAL=$(($VAL+1))
 	echo -n "flow $VAL" | nc 127.0.0.1 8080
       ;;
-   #KEY_VOLUMEDOWN)
    KEY_5)
 	VAL=$(echo -n "flow" | nc 127.0.0.1 8080)
 	VAL=$(($VAL-1))
 	echo -n "flow $VAL" | nc 127.0.0.1 8080
       ;;
+   KEY_6)
+	VAL=$(echo -n "temp" | nc 127.0.0.1 8080)
+	VAL=$(($VAL+1))
+	echo -n "temp $VAL" | nc 127.0.0.1 8080
+      ;;
+   KEY_7)
+	VAL=$(echo -n "temp" | nc 127.0.0.1 8080)
+	VAL=$(($VAL-1))
+	echo -n "temp $VAL" | nc 127.0.0.1 8080
+      ;;
+   KEY_8)
+        VAL=$(echo -n "load" | nc 127.0.0.1 8080)
+        VAL=$(($VAL+1))
+        [ $VAL -ge 255 ] && VAL=255
+	echo -n "load $VAL" | nc 127.0.0.1 8080
+      ;;
+   KEY_9)
+        VAL=$(echo -n "load" | nc 127.0.0.1 8080)
+        VAL=$(($VAL-1))
+        [ $VAL -le 0 ] && VAL=0
+        echo -n "load $VAL" | nc 127.0.0.1 8080
+      ;;
+   KEY_PLUS)
+        VAL=$(echo -n "intake" | nc 127.0.0.1 8080)
+        VAL=$(($VAL+1))
+        [ $VAL -ge 255 ] && VAL=255
+        echo -n "intake $VAL" | nc 127.0.0.1 8080
+      ;;
+   KEY_MINUS)
+        VAL=$(echo -n "intake" | nc 127.0.0.1 8080)
+        VAL=$(($VAL-1))
+        [ $VAL -le 0 ] && VAL=0
+        echo -n "intake $VAL" | nc 127.0.0.1 8080
+      ;;
    *)
      echo "invalid arg"
      ;;
 esac
+

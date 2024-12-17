9d583c4494f1efdb104b2003b979add3598914a1
hackboxguy <albert.david@gmail.com>
2024-12-17 11:46:17 +0100

    updated custom-patch.sh of raspi4-remote-kit to copy config.txt and cmdline.txt to openwrt location

--- configs/raspi4-remote-kit/patches/custom-patch.sh
+++ configs/raspi4-remote-kit/patches/custom-patch.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 cp configs/raspi4-remote-kit/patches/can.mk openwrt/package/kernel/linux/modules/
+cp configs/raspi4-remote-kit/rootfs_overlay/boot/config.txt openwrt/target/linux/bcm27xx/image/
+cp configs/raspi4-remote-kit/rootfs_overlay/boot/cmdline.txt openwrt/target/linux/bcm27xx/image/
